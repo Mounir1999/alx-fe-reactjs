8f921c0414de159bf0a064ef6fd4a0c3016db917
Mounir Akhsassi <mounirakhsassi@gmail.com>
2024-11-10 16:37:43 +0100

    userData

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,20 +1,15 @@
-import React from 'react';
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+// src/App.jsx
+import ProfilePage from './components/ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <WelcomeMessage />
-            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-            <Footer />
-        </div>
-    );
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
