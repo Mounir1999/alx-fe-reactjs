bbbcf2948ee28b9bd5c0843ca7430ecddc112260
Mounir Akhsassi <111659459+Mounir1999@users.noreply.github.com>
2024-11-10 17:31:45 +0100

    Delete alx-react-app-props directory

--- alx-react-app-props/src/UserContext.js
+++
@@ -1,8 +0,0 @@
-// src/UserContext.js
-import { createContext } from 'react';
-
-// Create the context for user data
-const UserContext = createContext();
-
-// Export the context for use in other components
-export default UserContext;
