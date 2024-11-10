fe985cf037d4991541706430310b04ed4b5c3411
Mounir Akhsassi <111659459+Mounir1999@users.noreply.github.com>
2024-11-10 15:39:04 +0100

    Update MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
-function MainContent() {
+function MainContent() { 
     return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
+        <main style={{ backgroundColor: '#f0f0f0', padding: '15px', borderRadius: '5px', textAlign: 'center' }}>
+            <p style={{ fontSize: '18px', color: '#333' }}>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
 }
